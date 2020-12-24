06be954760a32613b572a0acb177fcc3ed8b9a4c
kakatoji <kakatoji0@gmail.com>
2020-12-24 10:38:46 +0800

    dork.php

---
+++ dork.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(0);
+system("clear");
+function search($cek){
+header('Content-Type: application/json');
+$file =file_get_contents("https://www.google.com/search?q=".urlencode($cek));
+preg_match_all('/\/url\?q=(.*?)&amp/', $file, $hasil);
+return $hasil;
+}
+
+print "=============================\n";
+print "        Dorker google        \n";
+print "=============================\n";
+print "           kakatoji          \n";
+print "=============================\n";
+print "       Indo Ghost Cyber      \n";
+print "=============================\n";
+$cari=readline("[>]Input: ");
+print "==============================\n";
+$cek=search($cari);
+$i=1;
+foreach($cek[1] as $head){
+  echo "[".$i++."] ".$head."\n";
+}
